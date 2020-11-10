38f689bd9c4b757a23d99e05351bcbca30cbe5b3
joannetuukmangi <s11910041@student.unklab.ac.id>
2020-11-10 21:33:36 +0800

    first commit

---
+++ Pertemuan 20/index.js
@@ -0,0 +1,62 @@
+//variabel
+
+//let & cons
+
+// let _umur = 25;
+// _umur = 26;
+// // let nilai = 95.5;
+// // let sudahMenikah = false;
+// // let $nama = "John Doe";
+// // console.log(_umur);
+// // console.log($nama);
+
+//     let alamat;
+//     console.log(alamat);
+
+// // const tahunLahir = 2010;
+// // console.log(tahunLahir);
+
+// let angka1 = 10;
+// let angka2 = 20;
+// console.log(angka1+angka2);
+
+// let umur = 30;
+// let namaDepan = "John";
+// let namaBelakang = "Doe";
+// console.log("Nama Saya" + namaDepan + " " namaBelakang " " + umur " ");
+
+//ternary operator
+// let angka1 = 10;
+// let angka2 = 9;
+
+// console.log(angka1 <= angka2);
+
+//operator logika
+//let kondisi1 = 1<2;
+//let kondisi2 = true;
+
+//console.log(kondisi1 || kondisi2);
+//console.log(!kondisi1);
+
+//let berat = prompt("Masukan Berat: ");
+//console.log("Berat yg dimasukan adalah " + berat);
+//let tinggi = prompt("Masukan Tinggi: ");
+//console.log("Tinggi yg dimasukan adalah " + tinggi);
+
+//let bilangan1 = berat;
+//let bilangan2 = tinggi;
+
+//console.log(bilangan1 / bilangan2 * bilangan2);
+
+let berat = prompt("Masukan berat badan anda = kg");
+let tinggi = prompt("Masukan tinggi badan anda = cm");
+let bni = berat / (tinggi * tinggi);
+
+console.log("your BNI = " + bni);
+
+
+//exercise #7
+/*
+Buatlah program Javascript untuk menghitung BMI(Body Mass Index).
+Rumus = berat (kg) / (tinggi * tinggi)
+gunakan promt untuk meminta input user.*/
